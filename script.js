98fc4ef6877a3ade6dafed7c5a0b0e9694c4e8ea
knhu <93463539+knhu@users.noreply.github.com>
2022-07-31 00:08:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const net = new brain.NeuralNetwork()
+
+const data = [
+    {
+        input: {r: 0, g: 0, b: 0},
+        output: [1]
+    },
+    {
+        input: {r: 1, g: 1, b: 1},
+        output: [0]
+    }
+]
+
+net.train(data)
+
+const colorElement = document.getElementById('color')
+const guessElement = document.getElementById('guess')
+const whiteButton = document.getElementById('white-button')
+const blackButton = document.getElementById('black-button')
+const printButton = document.getElementById('print-button')
+let color
+setRandomColor()
+
+whiteButton.addEventListener('click', () => {
+    chooseColor(1)
+})
+
+blackButton.addEventListener('click', () => {
+    chooseColor(0)
+})
+
+printButton.addEventListener('click', print)
+
+function chooseColor(value) {
+    data.push({
+        input: color,
+        output: [value]
+    })
+    setRandomColor()
+}
+
+function print() {
+    console.log(JSON.stringify(data))
+}
+
+function setRandomColor() {
+    color = {
+        r: Math.random(),
+        g: Math.random(),
+        b: Math.random()
+    }
+    const guess = net.run(color)[0]
+    guessElement.style.color = guess > .5 ? '#FFF' : '#000'
+    colorElement.style.backgroundColor = `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255})`
+}
+
